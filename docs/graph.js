2525765d6aa84c03212129f26de79512a66ecc00
BoDee Angus <bodee.angus@gmail.com>
2024-02-25 15:54:01 -0700

    implemented canvas instead of svg

--- docs/graph.js
+++ docs/graph.js
@@ -1,168 +1,103 @@
-fetch('./graphData.json')
-  .then((response) => response.json())
-  .then((graphData) => {
-    const { nodes, links } = graphData;
+document.addEventListener('DOMContentLoaded', () => {
+  const pixelRatio = window.devicePixelRatio || 1;
 
-    // Select the SVG element and set its width and height
-    const svg = d3.select('svg'),
-      width = window.innerWidth,
-      height = window.innerHeight;
+  const canvas = document.querySelector('canvas');
+  const context = canvas.getContext('2d');
 
-    svg.attr('viewBox', `0 0 ${width} ${height}`);
+  // Displayed size
+  const displayedWidth = canvas.offsetWidth;
+  const displayedHeight = canvas.offsetHeight;
 
-    // Create the definitions for gradients
-    const defs = svg.append('defs');
+  // Set actual size in memory (scaled to account for pixel ratio)
+  canvas.width = displayedWidth * pixelRatio;
+  canvas.height = displayedHeight * pixelRatio;
 
-    // Create a group (`g`) element to contain all graph elements
-    const g = svg.append('g');
+  // Scale the context to ensure correct drawing operations
+  context.scale(pixelRatio, pixelRatio);
 
-    let link = g.append('g').attr('class', 'links').selectAll('path');
+  // Ensure canvas CSS size matches its displayed size (not its in-memory size)
+  canvas.style.width = displayedWidth + 'px';
+  canvas.style.height = displayedHeight + 'px';
 
-    let node = g.append('g').attr('class', 'nodes').selectAll('circle');
+  let transform = d3.zoomIdentity; // Initialize transform for zoom
 
-    // Define the zoom behavior with initial scaling and translation to center the graph
-    const zoomHandler = d3
-      .zoom()
-      .on('zoom', (event) => {
-        g.attr('transform', event.transform);
-      })
-      .scaleExtent([0.01, 10]);
-
-    svg
-      .call(zoomHandler)
-      .call(
-        zoomHandler.transform,
-        d3.zoomIdentity.translate(width / 2, height / 2).scale(1)
-      );
-
-    // Initialize the force simulation
-    const simulation = d3
-      .forceSimulation(nodes)
-      .force(
-        'link',
-        d3
-          .forceLink(links)
-          .id((d) => d.id)
-          .distance(0)
-          .strength(
-            (d) =>
-              (d.weight * d.weight * d.weight * d.weight * d.weight) / 100000000
-          )
-      )
-      .force('charge', d3.forceManyBody().strength(-1000).theta(1))
-      .force('center', d3.forceCenter(width / 2, height / 2))
-      .alphaDecay(0)
-      .on('tick', ticked);
-
-    // Function to handle the drag events
-    function dragstarted(event) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
-    }
-
-    function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
-    }
-
-    function dragended(event) {
-      if (!event.active) simulation.alphaTarget(0);
-      event.subject.fx = null;
-      event.subject.fy = null;
-    }
-
-    links.forEach((link, i) => {
-      const sourceColor = nodes.find(
-        (n) =>
-          n.id ===
-          (typeof link.source === 'object' ? link.source.id : link.source)
-      ).color;
-      const targetColor = nodes.find(
-        (n) =>
-          n.id ===
-          (typeof link.target === 'object' ? link.target.id : link.target)
-      ).color;
-
-      const gradient = defs
-        .append('linearGradient')
-        .attr('id', `gradient-${i}`)
-        .attr('gradientUnits', 'userSpaceOnUse')
-        .attr('x1', 0)
-        .attr('y1', 0)
-        .attr('x2', 0)
-        .attr('y2', 0);
-
-      gradient
-        .append('stop')
-        .attr('offset', '0%')
-        .attr('stop-color', sourceColor);
-
-      gradient
-        .append('stop')
-        .attr('offset', '100%')
-        .attr('stop-color', targetColor);
-    });
-
-    // Create nodes and links with initial properties
-    link = link
-      .data(links)
-      .enter()
-      .append('path')
-      .attr('stroke-width', 5)
-      .attr('stroke', (d, i) => `url(#gradient-${i})`);
-
-    const nodeEnter = g
-      .append('g')
-      .attr('class', 'nodes')
-      .selectAll('g')
-      .data(nodes)
-      .enter()
-      .append('g')
-      .call(
-        d3
-          .drag()
-          .on('start', dragstarted)
-          .on('drag', dragged)
-          .on('end', dragended)
-      );
-
-    // Append a circle to each 'g'
-    nodeEnter
-      .append('circle')
-      .attr('r', (d) => d.radius)
-      .attr('fill', (d) => d.color);
-
-    // Append text to each 'g'
-    nodeEnter
-      .append('text')
-      .attr('dy', '.35em') // Vertically center text
-      .attr('text-anchor', 'middle') // Horizontally center text
-      .attr('font-weight', 'bold')
-      .style('fill', 'black') // Text color
-      .text((d) => d.id);
-
-    // Ticked function to update positions and gradient orientations
-    function ticked() {
-      link
-        .attr(
-          'd',
-          (d) => `M${d.source.x},${d.source.y} L${d.target.x},${d.target.y}`
+  fetch('./graphData.json')
+    .then((response) => response.json())
+    .then((graphData) => {
+      const simulation = d3
+        .forceSimulation(graphData.nodes)
+        .force(
+          'link',
+          d3
+            .forceLink(graphData.links)
+            .id((d) => d.id)
+            .distance(0)
+            .strength(
+              (d) =>
+                (d.weight * d.weight * d.weight * d.weight * d.weight) /
+                100000000
+            )
         )
-        .each(function (d, i) {
-          defs
-            .select(`#gradient-${i}`)
-            .attr('x1', d.source.x)
-            .attr('y1', d.source.y)
-            .attr('x2', d.target.x)
-            .attr('y2', d.target.y);
+        .force('charge', d3.forceManyBody().strength(-1000).theta(1))
+        .force(
+          'center',
+          d3.forceCenter(displayedWidth / 2, displayedHeight / 2)
+        )
+        .alphaDecay(0)
+        .on('tick', draw); // Call draw function on tick
+
+      const zoom = d3
+        .zoom()
+        .scaleExtent([0.1, 10])
+        .on('zoom', (event) => {
+          transform = event.transform;
+          draw(); // Call draw function to apply zoom
         });
 
-      node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
+      d3.select(canvas).call(zoom);
 
-      nodeEnter.attr('transform', (d) => `translate(${d.x}, ${d.y})`);
-    }
+      function draw() {
+        context.save();
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.translate(transform.x, transform.y);
+        context.scale(transform.k, transform.k);
 
-    simulation.on('tick', ticked);
-  })
-  .catch((error) => console.error('Error loading the graph data:', error));
+        // Drawing links
+        graphData.links.forEach((link) => {
+          // Assuming link.source and link.target are objects with x, y, and color properties
+          const gradient = context.createLinearGradient(
+            link.source.x,
+            link.source.y,
+            link.target.x,
+            link.target.y
+          );
+          gradient.addColorStop(0, link.source.color);
+          gradient.addColorStop(1, link.target.color);
+
+          context.beginPath();
+          context.moveTo(link.source.x, link.source.y);
+          context.lineTo(link.target.x, link.target.y);
+          context.strokeStyle = gradient; // Use the gradient for the stroke style
+          context.lineWidth = 5; // Increase the line width as needed
+          context.stroke();
+        });
+
+        // Drawing nodes
+        graphData.nodes.forEach((node) => {
+          context.beginPath();
+          context.arc(node.x, node.y, node.radius, 0, 2 * Math.PI, true);
+          context.fillStyle = node.color;
+          context.fill();
+
+          // Draw the node name
+          context.fillStyle = 'black'; // Text color
+          context.textAlign = 'center'; // Align text horizontally centered
+          context.textBaseline = 'middle'; // Align text vertically centered
+          context.fillText(node.id, node.x, node.y);
+        });
+
+        context.restore();
+      }
+    })
+    .catch((error) => console.error('Error fetching graph data:', error));
+});
